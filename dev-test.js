3268f2c35f3b0056442928dacf474844c8c30b0d
Tejas Wadiwala <tejaswadiwala@gmail.com>
2021-07-01 22:24:30 -0400

    working blockchain prototype first commit

---
+++ dev-test.js
@@ -0,0 +1,13 @@
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
+
+
+
+/* const Blockchain = require('./blockchain');
+
+const bc = new Blockchain();
+
+for (let i=0; i<10; i++) {
+    console.log(bc.addBlock(`foo ${i}`).toString());
+} */
